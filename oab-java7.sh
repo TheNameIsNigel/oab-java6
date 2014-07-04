a3dc3557b13ae074bbafac9c07c5e1049a9470c8
Ryan Norris <thenameisnigel@users.noreply.github.com>
2014-07-04 11:17:54 -0400

    Update to the latest JVM (Java7u60).

--- oab-java7.sh
+++ oab-java7.sh
@@ -10,8 +10,8 @@ echo
 clear
 echo Please wait while we intialize the application...
 echo Checking for previous Java versions...
-ls /usr/lib/jvm >> /tmp/oabjava.sh
-cat /tmp/oabjava.sh | grep jvm*
+ls /usr/lib/jvm >> /tmp/oabjava.ngel
+cat /tmp/oabjava.ngel | grep jvm*
 sudo apt-get remove openjdk*
 clear
 echo OpenJDK has been uninstalled if it was installed.
@@ -37,9 +37,9 @@ clear
 echo Ill be pulling the files from Oracle necessary to install Java...
 echo
 echo 
-wget --no-check-certificate --no-cookies - --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/7u51-b13/jdk-7u51-linux-x64.tar.gz
+wget --no-check-certificate --no-cookies - --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/7u60-b19/jdk-7u60-linux-x64.tar.gz
 echo Download Completed, lets set some permissions...
-chmod 777 jdk-7u51-linux-x64.tar.gz
+chmod 777 jdk-7u60-linux-x64.tar.gz
 echo
 echo
 echo
@@ -49,7 +49,7 @@ echo
 echo
 echo Java has been unpacked. Please hold on...
 sudo mkdir -p /usr/lib/jvm/
-sudo mv jdk1.7.0_51 /usr/lib/jvm/jdk1.7.0_51
+sudo mv jdk1.7.0_60 /usr/lib/jvm/jdk1.7.0_60
 clear
 echo Oracle JVM has been downloaded and is ready to install.
 clear
@@ -59,10 +59,10 @@ echo
 echo Okay, the hard part is done. Lets set Oracle Java as the default
 echo java.
 wait 5
-sudo update-alternatives --install /usr/bin/javac javac /usr/lib/jvm/jdk1.7.0_51/bin/javac 1
-sudo update-alternatives --install /usr/bin/java java /usr/lib/jvm/jdk1.7.0_51/bin/java 1
-sudo update-alternatives --install /usr/bin/jar jar /usr/lib/jvm/jdk1.7.0_51/bin/jar 1
-sudo update-alternatives --install /usr/bin/javaws javaws /usr/lib/jvm/jdk1.7.0_51/bin/javaws 1
+sudo update-alternatives --install /usr/bin/javac javac /usr/lib/jvm/jdk1.7.0_60/bin/javac 1
+sudo update-alternatives --install /usr/bin/java java /usr/lib/jvm/jdk1.7.0_60/bin/java 1
+sudo update-alternatives --install /usr/bin/jar jar /usr/lib/jvm/jdk1.7.0_60/bin/jar 1
+sudo update-alternatives --install /usr/bin/javaws javaws /usr/lib/jvm/jdk1.7.0_60/bin/javaws 1
 sudo update-alternatives --config javac 1
 sudo update-alternatives --config java 1
 sudo update-alternatives --config javaws 1
@@ -71,7 +71,7 @@ ls -la /etc/alternatives/java*
 clear
 echo Done. Please wait..
 wait 3
-JAVA_HOME=/usr/lib/jvm/jdk1.7.0_51
+JAVA_HOME=/usr/lib/jvm/jdk1.7.0_60
 MOZILLA_HOME=~/.mozilla
 mkdir $MOZILLA_HOME/plugins
 ln -s $JAVA_HOME/jre/lib/amd64/libnpjp2.so $MOZILLA_HOME/plugins
